f2347ec330ffc109405b681021b8637c23058c9f
Vina Rianti <vinarianti@singaporepower.com.sg>
2016-11-09 20:37:11 +0800

    Initial Commit

--- PlayHue/ViewController.swift
+++ PlayHue/ViewController.swift
@@ -8,8 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBAction func capture(_ sender: UIButton) {
+        
+        let imagePicker: UIImagePickerController = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            print("Application cannot access the camera.")
+            return
+        }
+        
+        guard (UIImagePickerController.availableCaptureModes(for: .rear) != nil) else {
+            print("Camera not found")
+            return
+        }
+        
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera
+        imagePicker.cameraCaptureMode = .photo
+        imagePicker.cameraDevice = .rear
+        imagePicker.modalPresentationStyle = .overCurrentContext
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true, completion: nil)
+        
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.image.contentMode = .scaleAspectFit
+            self.image.image = pickedImage
+            let averageColor = UIImage.areaAverage(pickedImage)
+            print(averageColor)
+            self.view.backgroundColor = averageColor()
+        }
+        
+        
+        
+//        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+//        let imageData = UIImageJPEGRepresentation(image, 0.8)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,3 +67,29 @@ class ViewController: UIViewController {
 
 }
 
+extension UIImage {
+    
+    func areaAverage() -> UIColor {
+        
+        var bitmap = [UInt8](repeating: 0, count: 4)
+        
+        let context = CIContext(options: nil)
+        let cgImg = context.createCGImage(CoreImage.CIImage(cgImage: self.cgImage!), from: CoreImage.CIImage(cgImage: self.cgImage!).extent)
+        
+        let inputImage = CIImage(cgImage: cgImg!)
+        let extent = inputImage.extent
+        let inputExtent = CIVector(x: extent.origin.x, y: extent.origin.y, z: extent.size.width, w: extent.size.height)
+        let filter = CIFilter(name: "CIAreaAverage", withInputParameters: [kCIInputImageKey: inputImage, kCIInputExtentKey: inputExtent])!
+        let outputImage = filter.outputImage!
+        let outputExtent = outputImage.extent
+        assert(outputExtent.size.width == 1 && outputExtent.size.height == 1)
+        
+        // Render to bitmap.
+        context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: kCIFormatRGBA8, colorSpace: CGColorSpaceCreateDeviceRGB())
+        
+        // Compute result.
+        let result = UIColor(red: CGFloat(bitmap[0]) / 255.0, green: CGFloat(bitmap[1]) / 255.0, blue: CGFloat(bitmap[2]) / 255.0, alpha: CGFloat(bitmap[3]) / 255.0)
+        return result
+    }
+    
+}
